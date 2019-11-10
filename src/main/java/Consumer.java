2a95d8e8fb67f65ab6f64a41103962870d93f2b1
Nicolas <nicoga97@gmail.com>
2019-11-10 14:40:09 -0500

    Consumer and producer created

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,56 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public  class Consumer implements Runnable, ExceptionListener {
+
+    public static void main(String[] args){
+        Consumer consumer = new Consumer();
+        consumer.run();
+    }
+
+    public void run() {
+        try {
+
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616?jms.useAsyncSend=true");
+
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection("smx", "smx");
+            connection.start();
+
+            connection.setExceptionListener(this);
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue("events");
+
+            // Create a MessageConsumer from the Session to the Topic or Queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // Wait for a message
+            Message message = consumer.receive(1000);
+
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = textMessage.getText();
+                System.out.println("Received: " + text);
+            } else {
+                System.out.println("Received: " + message);
+            }
+
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void onException(JMSException ex) {
+        System.out.println("JMS Exception occured.  Shutting down client.");
+    }
+}
